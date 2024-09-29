efda8d4fa1f1bfae839cf5aa92ac1ae08fa0cad3
root <root@DESKTOP-B19RQ5P>
2024-09-29 20:55:50 +0300

    made response json

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,20 +6,20 @@ exports.getStatus = (req, res) => {
   const redisValue = redis.isAlive();
   const dbValue = mongo.isAlive();
   if (redisValue && dbValue) {
-    return res.status(200).send({ redis: redisValue, db: dbValue });
+    return res.status(200).json({ redis: redisValue, db: dbValue });
   }
 };
 
 exports.getStats = (req, res) => {
   const stats = {};
-  mongo.nbUsers()
+  return mongo.nbUsers()
     .then((value) => {
       stats.users = value;
       return mongo.nbFiles();
     })
     .then((value) => {
       stats.files = value;
-      return res.status(200).send(stats);
+      return res.status(200).json(stats);
     })
     .catch((error) => { // eslint-disable-line no-unused-vars
       return res.status(500).send('Falied to fetch stat');
