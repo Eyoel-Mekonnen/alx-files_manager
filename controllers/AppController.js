7259e4e2a0ec085fcb834f3394ece47de2470c90
root <root@DESKTOP-B19RQ5P>
2024-09-30 10:22:19 +0300

    added a class to check

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,22 +2,25 @@ const redis = require('../utils/redis');
 
 const mongo = require('../utils/db');
 
-exports.getStatus = (req, res) => {
-  const redisValue = redis.isAlive();
-  const dbValue = mongo.isAlive();
-  return res.status(200).json({ redis: redisValue, db: dbValue });
-};
+class AppController {
+  static getStatus(req, res) {
+    const redisValue = redis.isAlive();
+    const dbValue = mongo.isAlive();
+    return res.status(200).json({ redis: redisValue, db: dbValue });
+  }
 
-exports.getStats = (req, res) => {
-  const stats = {};
-  return mongo.nbUsers()
-    .then((value) => {
-      stats.users = value;
-      return mongo.nbFiles();
-    })
-    .then((value) => {
-      stats.files = value;
-      return res.status(200).json(stats);
-    })
-    .catch(() => res.status(500).send('Falied to fetch stat'));
-};
+  static getStats(req, res) {
+    const stats = {};
+    return mongo.nbUsers()
+      .then((value) => {
+        stats.users = value;
+        return mongo.nbFiles();
+      })
+      .then((value) => {
+        stats.files = value;
+        return res.status(200).json(stats);
+      })
+      .catch(() => res.status(500).send('Falied to fetch stat'));
+  }
+}
+module.exports = AppController;
