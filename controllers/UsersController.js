bac4b69689fe3313f0531400c71f5d62b82750f7
root <root@DESKTOP-B19RQ5P>
2024-09-30 19:58:44 +0300

    ouput made json and corrected format

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -13,17 +13,17 @@ exports.postNew = (req, res) => {
   const email = req.body ? req.body.email : null;
   const password = req.body ? req.body.password : null;
   if (!email) {
-    res.status(400).send({ error: 'Missing email' });
+    res.status(400).json({ error: 'Missing email' });
     return;
   }
   if (!password) {
-    res.status(400).send({ error: 'Missing password' });
+    res.status(400).json({ error: 'Missing password' });
     return;
   }
   mongo.db.collection('users').findOne({ email })
     .then((value) => {
       if (value) {
-        return res.status(200).send('Already exist');
+        return res.status(200).json({ error: 'Already exist' });
       }
       const passwordHashed = sha1(password);
       const emailPasswordObject = { email, password: passwordHashed };
@@ -34,30 +34,29 @@ exports.postNew = (req, res) => {
         const id = insertedData.insertedId;
         return res.status(200).send({ id, email });
       }
-      return res.status(400).send({ error: 'Internal Server Error' });
+      return res.status(400).json({ error: 'Internal Server Error' });
     })
-    .catch(() => res.status(400).send({ error: 'Internal Server Error' }));
+    .catch(() => res.status(400).json({ error: 'Internal Server Error' }));
 };
 
 exports.getMe = (req, res) => {
   const xToken = req.headers['x-token'];
   if (!xToken) {
-    return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Unauthorized' });
   }
   const key = `auth_${xToken}`;
-  console.log(`I am the key who is going to retreive ${key}`);
   redis.get(key)
     .then((userID) => {
       if (!userID) {
-        return res.status(401).send({ error: 'Unauthorized' });
+        return res.status(401).json({ error: 'Unauthorized' });
       }
       return mongo.db.collection('users').findOne({ _id: ObjectId(userID) });
     })
     .then((data) => {
       if (!data) {
-        return res.status(401).send({ error: 'Unauthorized' });
+        return res.status(401).json({ error: 'Unauthorized' });
       }
-      return res.status(200).send({ id: data._id, email: data.email });
+      return res.status(200).json({ id: data._id, email: data.email });
     })
-    .catch(() => res.status(401).send({ error: 'Unauthorized' }));
+    .catch(() => res.status(401).json({ error: 'Unauthorized' }));
 };
