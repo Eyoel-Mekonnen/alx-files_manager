3ee07ae7f9de0e826d03348c91492cf2c952dd91
root <root@DESKTOP-B19RQ5P>
2024-09-30 19:38:30 +0300

    corrected port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.DB_PORT || 5000;
+const port = process.env.PORT || 5000;
 const index = require("./routes/index")
 
 app.use(express.json()); // This needs to be above any route handlers.
